56bac2797ede1dee05945bbed2332848be49d0b5
Si-143 <30472917+Si-143@users.noreply.github.com>
2017-09-24 15:43:27 +0100

    Add files via upload

---
+++ Models/ItemContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace API.Models
+{
+	public class ItemContext : DbContext
+	{
+		public ItemContext(DbContextOptions<ItemContext> options)
+			: base(options)
+		{
+		}
+        // create the basket to store items. 
+		public DbSet<Items> BasketItems { get; set; }
+
+	}
+}
---
+++ Models/Items.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace API.Models
+{
+    // create the Item object, which contains ID,Name and Quantity 
+    public class Items
+    {
+        public int Id { get; set; }
+		public string Name { get; set; }
+		public int Quantity { get; set; }
+    }
+}
